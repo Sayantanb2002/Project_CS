d4b3249d8c0fd1648514bbaffc984797269a9eca
Sayantan <Sayantanb595@gmail.com>
2025-05-18 12:33:32 +0530

    update

--- hooks/useGetCallById.ts
+++ hooks/useGetCallById.ts
@@ -1,25 +1,33 @@
-import { Call, useStreamVideoClient } from "@stream-io/video-react-sdk"
-import { useEffect, useState } from "react"
+import { Call, useStreamVideoClient } from "@stream-io/video-react-sdk";
+import { useEffect, useState } from "react";
 
-export const useGetCallById = (id: string | string[]) => {
-    const [call , setCall] = useState<Call>()
-    const [isCallLoading , setIsCallLoading] = useState(true)
+export const useGetCallById = (id: string | string[] | undefined) => {
+  const [call, setCall] = useState<Call>();
+  const [isCallLoading, setIsCallLoading] = useState(true);
 
-    const client = useStreamVideoClient(); // to get the current client stream
+  const client = useStreamVideoClient(); // to get the current client stream
 
-    useEffect(() => {
-        if(!client) return;
+  useEffect(() => {
+    if (!client || typeof id !== 'string') return;
 
-        const loadCall = async () => {
-            const { calls } = await client.queryCalls({
-                filter_conditions: { id },
-            })
+    const loadCall = async () => {
+      try {
+        // https://getstream.io/video/docs/react/guides/querying-calls/#filters
+        const { calls } = await client.queryCalls({
+          filter_conditions: { id },
+        });
 
-            if(calls.length > 0) setCall(calls[0]);
-            setIsCallLoading(false)
-        }
-        loadCall();
-    } , [client , id])
+        if (calls.length > 0) setCall(calls[0]);
 
-    return { call , isCallLoading }
-}
+        setIsCallLoading(false);
+      } catch (error) {
+        console.error(error);
+        setIsCallLoading(false);
+      }
+    };
+
+    loadCall();
+  }, [client, id]);
+
+  return { call, isCallLoading };
+};
